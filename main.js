94576bb1d829789d0793df8b0251d6b4e67e79ea
umesh354 <deshmukhumesh354@gmail.com>
2024-03-04 22:12:26 +0530

    Header section and scroll complete

---
+++ main.js
@@ -0,0 +1,6 @@
+// navbar eeffect on scroll
+
+window.addEventListener("scroll", function(){
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
