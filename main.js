d4338b2bad8916b7847fcef9b45c4efc2ca0ea54
umesh354 <deshmukhumesh354@gmail.com>
2024-03-05 13:37:59 +0530

    COnfigured popup properly for sevices section

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
 // navbar eeffect on scroll
 
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
     const header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 })
+
+// service section inside - modal
+
+const serviceModals = document.querySelectorAll(".service-modal");
+const learnMoreBtns = document.querySelectorAll(".learn-more-btn");
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+function openModal(modalIndex) {
+    serviceModals[modalIndex].classList.add("active");
+}
+
+function closeModal() {
+    serviceModals.forEach((modalView) => {
+        modalView.classList.remove("active");
+    });
+}
+
+learnMoreBtns.forEach((learnMoreBtn, i) => {
+    learnMoreBtn.addEventListener("click", () => {
+        openModal(i);
+    });
+});
+
+modalCloseBtns.forEach((modalCloseBtn) => {
+    modalCloseBtn.addEventListener("click", () => {
+        closeModal();
+    });
+});
