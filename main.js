45b0e955c75aba7c9a91bfd96bde962fc20ba0a4
umesh354 <deshmukhumesh354@gmail.com>
2024-03-05 21:01:37 +0530

    openpopup for portfolio working fine

--- main.js
+++ main.js
@@ -32,3 +32,27 @@ modalCloseBtns.forEach((modalCloseBtn) => {
         closeModal();
     });
 });
+
+// portfolio section
+
+const portfolioModals = document.querySelectorAll(".portfolio-model");
+const imgCards = document.querySelectorAll(".img-card");
+const portfolioCloseBtns = document.querySelectorAll(".portfolio-close-btn");
+
+var portfolioModal = function (modalClick) {
+  portfolioModals[modalClick].classList.add("active");
+}
+
+imgCards.forEach((imgCard, i) => {
+  imgCard.addEventListener("click", () => {
+    portfolioModal(i);
+  });
+});
+
+portfolioCloseBtns.forEach((portfolioCloseBtn) => {
+  portfolioCloseBtn.addEventListener("click", () => {
+    portfolioModals.forEach((portfolioModalView) => {
+      portfolioModalView.classList.remove("active");
+    });
+  });
+});
