dfa51927f572b992165d48b795d71fc363bb0c52
bhushan <deshmukhumesh354@gmail.com>
2024-03-07 12:03:36 +0530

    Formatted document

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // navbar eeffect on scroll
 
 window.addEventListener("scroll", function () {
-    const header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
 })
 
 // service section inside - modal
@@ -12,25 +12,25 @@ const learnMoreBtns = document.querySelectorAll(".learn-more-btn");
 const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
 
 function openModal(modalIndex) {
-    serviceModals[modalIndex].classList.add("active");
+  serviceModals[modalIndex].classList.add("active");
 }
 
 function closeModal() {
-    serviceModals.forEach((modalView) => {
-        modalView.classList.remove("active");
-    });
+  serviceModals.forEach((modalView) => {
+    modalView.classList.remove("active");
+  });
 }
 
 learnMoreBtns.forEach((learnMoreBtn, i) => {
-    learnMoreBtn.addEventListener("click", () => {
-        openModal(i);
-    });
+  learnMoreBtn.addEventListener("click", () => {
+    openModal(i);
+  });
 });
 
 modalCloseBtns.forEach((modalCloseBtn) => {
-    modalCloseBtn.addEventListener("click", () => {
-        closeModal();
-    });
+  modalCloseBtn.addEventListener("click", () => {
+    closeModal();
+  });
 });
 
 // portfolio section
@@ -61,37 +61,37 @@ portfolioCloseBtns.forEach((portfolioCloseBtn) => {
 // navigation active on pagr scroll
 
 window.addEventListener("scroll", () => {
-    const sections = document.querySelectorAll("section");
-    const scrollY = window.pageYOffset;
-  
-    sections.forEach(current => {
-      const sectionHeight = current.offsetHeight;
-      const sectionTop = current.offsetTop - 50;
-      const id = current.getAttribute("id");
-  
-      if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
-        document.querySelector(".nav-items a[href*=" + id + "]").classList.add("active");
-      } else {
-        document.querySelector(".nav-items a[href*=" + id + "]").classList.remove("active");
-      }
-    });
+  const sections = document.querySelectorAll("section");
+  const scrollY = window.pageYOffset;
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    const id = current.getAttribute("id");
+
+    if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+      document.querySelector(".nav-items a[href*=" + id + "]").classList.add("active");
+    } else {
+      document.querySelector(".nav-items a[href*=" + id + "]").classList.remove("active");
+    }
   });
-  
+});
 
-  // website dark mode
 
-  const themeBtn = document.querySelector(".theme-btn");
+// website dark mode
 
-  themeBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark-theme");
-    themeBtn.classList.toggle("moon");
+const themeBtn = document.querySelector(".theme-btn");
 
-    localStorage.setItem("saved-theme", getCurrentTheme());
-    localStorage.setItem("saved-icon", getCurrentIcon());
-  });
+themeBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+  themeBtn.classList.toggle("moon");
 
-  const getCurrentTheme = () => localStorage.getItem("saved-theme");
-  const getCurrentIcon = () => localStorage.getItem("saved-icom");
+  localStorage.setItem("saved-theme", getCurrentTheme());
+  localStorage.setItem("saved-icon", getCurrentIcon());
+});
+
+const getCurrentTheme = () => localStorage.getItem("saved-theme");
+const getCurrentIcon = () => localStorage.getItem("saved-icom");
 
 
 // if(savedTheme){
@@ -117,30 +117,30 @@ closeBtn.addEventListener("click", () => {
 
 navItem.forEach((navItem) => {
   navItem.addEventListener("click", () => {
-      navigation.classList.remove("active");
+    navigation.classList.remove("active");
   })
 })
 
 // scroll reveal libraru
 
-ScrollReveal ({
+ScrollReveal({
   reset: true,
   distance: '60px',
   duration: 1000,
   delay: 50
 })
 
-ScrollReveal().reveal('.home .info h2, .section-title-01, section-title-02', { delay: 50, origin: 'right'});
-ScrollReveal().reveal('.home .info h3, .home .info p, .about-info .btn', { delay: 50, origin: 'left'});
-ScrollReveal().reveal('.home .info .btn', { delay: 50, origin: 'bottom'});
+ScrollReveal().reveal('.home .info h2, .section-title-01, section-title-02', { delay: 50, origin: 'right' });
+ScrollReveal().reveal('.home .info h3, .home .info p, .about-info .btn', { delay: 50, origin: 'left' });
+ScrollReveal().reveal('.home .info .btn', { delay: 50, origin: 'bottom' });
 
-ScrollReveal().reveal('.media-icons i, .contact-left li', { delay: 50, origin: 'left'});
-ScrollReveal().reveal('.home-img, .about-img', { delay: 50, origin: 'bottom'});
+ScrollReveal().reveal('.media-icons i, .contact-left li', { delay: 50, origin: 'left' });
+ScrollReveal().reveal('.home-img, .about-img', { delay: 50, origin: 'bottom' });
 
-ScrollReveal().reveal('.about .description', { delay: 50, origin: 'right'});
+ScrollReveal().reveal('.about .description', { delay: 50, origin: 'right' });
 
-ScrollReveal().reveal('.about .professional-list li', { delay: 50, origin: 'right'});
+ScrollReveal().reveal('.about .professional-list li', { delay: 50, origin: 'right' });
 
-ScrollReveal().reveal('.skills-description, .service-description, .contact-card, contact-left-h2', { delay: 50, origin: 'left'});
+ScrollReveal().reveal('.skills-description, .service-description, .contact-card, contact-left-h2', { delay: 50, origin: 'left' });
 
-ScrollReveal().reveal('.experience-card, .service-card, .education, .portfolio .img-card', { delay: 50, origin: 'left'});
+ScrollReveal().reveal('.experience-card, .service-card, .education, .portfolio .img-card', { delay: 50, origin: 'left' });
