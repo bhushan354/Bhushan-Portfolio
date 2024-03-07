6e548b29c44bad51b1d24be34a8f7bc0c702c4d0
bhushan <deshmukhumesh354@gmail.com>
2024-03-07 08:50:11 +0530

    scroll reveal is working fine only for main name

--- main.js
+++ main.js
@@ -120,3 +120,14 @@ navItem.forEach((navItem) => {
       navigation.classList.remove("active");
   })
 })
+
+// scroll reveal libraru
+
+ScrollReveal ({
+  reset: true,
+  distance: '60px',
+  duration: 2500,
+  delay: 100
+})
+
+ScrollReveal().reveal('.home .info h2', { delay: 400, origin: 'left'});
